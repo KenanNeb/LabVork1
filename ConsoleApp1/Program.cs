c079d8de71202364a1892bbe7dca6be90646c302
NXbi_av4i <NXbi_av4i@STHQ0116-09>
2022-11-21 22:00:18 +0400

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,73 @@
+﻿using ConsoleApp1;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Text.Json;
+
+var ip = IPAddress.Loopback;
+var port = 27001;
+var listener = new TcpListener(ip, port);
+listener.Start();
+
+while (true)
+{
+    var client = listener.AcceptTcpClient();
+    var stream = client.GetStream();
+    var br = new BinaryReader(stream);
+    var bw = new BinaryWriter(stream);
+    while (true)
+    {
+        var input = br.ReadString();
+        var command = JsonSerializer.Deserialize<Command>(input);
+        if (command == null) continue;
+
+        Console.WriteLine(command.Text);
+        Console.WriteLine(command.Param);
+        switch (command.Text)
+        {
+            case Command.ProcessList:
+                var processes = Process.GetProcesses();
+                var processesNames = JsonSerializer.Serialize(processes.Select(p => p.ProcessName));
+                bw.Write(processesNames);
+                break;
+            case Command.Run:
+                bool check  = false;
+                try
+                {
+                    Process.Start(command.Param);
+                }
+                catch (Exception)
+                {
+                    check = true;                    
+                }
+                if (check == true)
+                {
+                    bw.Write(JsonSerializer.Serialize<bool>(false));
+                }
+                else 
+                {
+                    bw.Write(JsonSerializer.Serialize<bool>(true));
+                }
+                break;
+            case Command.Kill:
+                int ch = 0;
+                Process[] pcs = Process.GetProcesses();
+                foreach (var item in pcs)
+                {
+                    if (item.ProcessName == command.Param)
+                    {
+                        item.Kill();
+                        ch = 1;
+                    }
+                }
+                if (ch == 0)
+                    bw.Write(JsonSerializer.Serialize<bool>(false));
+                else if (ch == 1)
+                    bw.Write(JsonSerializer.Serialize<bool>(true));
+                break;
+
+            default:
+                break;
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,74 @@
+﻿using ConsoleApp2;
+using System.Data;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Text.Json;
+
+var ip = IPAddress.Loopback;
+var port = 27001;
+var client = new TcpClient();
+client.Connect(ip, port);
+var stream = client.GetStream();
+var br = new BinaryReader(stream);
+var bw = new BinaryWriter(stream);
+Command command = null;
+string responce = null;
+
+while (true)
+{
+	Console.WriteLine("Write Command or HELP");
+    var str = Console.ReadLine().ToUpper();
+	if (str == "HELP")
+	{
+		Console.WriteLine(Command.ProcessList);
+		Console.WriteLine($"{Command.Run} <process_name>");
+        Console.WriteLine($"{Command.Kill} <process_name>");
+		Console.WriteLine("HELP");
+		Console.ReadKey();
+		Console.Clear();
+		continue;
+    }
+	var input = str.Split(' ');
+	switch (input[0])
+	{
+		case Command.ProcessList:
+			command = new Command { Text = input[0] };
+			bw.Write(JsonSerializer.Serialize(command));
+			responce = br.ReadString();
+			var processesName = JsonSerializer.Deserialize<string[]>(responce);
+			foreach (var process in processesName)
+			{
+				Console.WriteLine(process);
+			}
+			Console.ReadKey();
+			Console.Clear();
+			break;
+        
+        case Command.Run:
+            command = new Command { Text = input[0], Param = input[1] };
+            bw.Write(JsonSerializer.Serialize(command));
+            responce = br.ReadString();
+            if (JsonSerializer.Deserialize<bool>(responce) == true)
+                Console.WriteLine("Process run oldu");
+            else
+                Console.WriteLine("Process run olmadi");
+            Console.ReadKey();
+            Console.Clear();
+            break;
+
+        case Command.Kill:
+            command = new Command { Text = input[0], Param = input[1].ToLower() };
+            bw.Write(JsonSerializer.Serialize(command));
+            responce = br.ReadString();
+            if (JsonSerializer.Deserialize<bool>(responce) == true)
+                Console.WriteLine("Process succeed");
+            else
+                Console.WriteLine("Process faild");
+            Console.ReadKey();
+            Console.Clear();
+            break;
+        default:
+			break;
+	}
+}
